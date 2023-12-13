b9bbf7365318d54682c8683536f05d0a6097508f
vas-sh <vas.shopuk@gmail.com>
2023-12-13 20:02:28 +0200

    created buttons Add subject and Add student

--- main.go
+++ main.go
@@ -86,40 +86,70 @@ func AvgMarks(rw http.ResponseWriter, r *http.Request) {
 }
 
 func createMarks(rw http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		name := r.FormValue("student_id")
-		lesson := r.FormValue("subject_id")
-		point := r.FormValue("value")
+	log.Println("createMark", r.Method)
 
-		log.Println("new mark: student_id", name, "subject_id", lesson, "value", point)
+	switch r.Method {
+	case http.MethodPost:
+		studentID := r.FormValue("student_id")
+		subjectID := r.FormValue("subject_id")
+		value := r.FormValue("value")
+		id := r.FormValue("id")
 
-		if err := db.Exec("INSERT INTO mark(student_id, subject_id, value) VALUES(?, ?, ?)",
-			name, lesson, point).Error; err != nil {
-			http.Error(rw, err.Error(), 400)
-			return
+		if id == "" || id == "0" {
+			log.Println("new mark: name", studentID, "lesson", subjectID, "point", value)
+			if err := db.Exec("INSERT INTO mark (ID, student_id, subject_id, value) VALUES(DEFAULT, ?, ?, ?)",
+				studentID, subjectID, value).Error; err != nil {
+				http.Error(rw, err.Error(), 400)
+				return
+			}
+		} else {
+			log.Println("update mark: student_id", studentID, "subject_id", subjectID, "value", value, "id", id)
+			if err := db.Exec("UPDATE mark SET student_id = ?, subject_id = ?, value = ? WHERE id = ?",
+				studentID, subjectID, value, id).Error; err != nil {
+				http.Error(rw, err.Error(), 400)
+				return
+			}
+
 		}
 		getMarks(rw, r)
 		return
-	}
 
-	var subjects []Subject
-	if err := db.Raw("SELECT * FROM subject").Scan(&subjects).Error; err != nil {
-		http.Error(rw, err.Error(), 400)
-		return
-	}
+	case http.MethodGet:
+		var subjects []Subject
+		if err := db.Raw("SELECT * FROM subject").Scan(&subjects).Error; err != nil {
+			http.Error(rw, err.Error(), 400)
+			return
+		}
 
-	var students []Student
-	if err := db.Raw("SELECT * FROM student").Scan(&students).Error; err != nil {
-		http.Error(rw, err.Error(), 400)
-		return
-	}
+		var students []Student
+		if err := db.Raw("SELECT * FROM student").Scan(&students).Error; err != nil {
+			http.Error(rw, err.Error(), 400)
+			return
+		}
+		if id := r.URL.Query().Get("id"); id != "" {
+			var mark Mark
+			if err := db.Raw("SELECT * FROM mark WHERE id = ?", id).Scan(&mark).Error; err != nil {
+				http.Error(rw, err.Error(), 400)
+				return
+			}
+			renderTemplate("html/update-mark.html", rw, struct {
+				Mark     Mark
+				Students []Student
+				Subjects []Subject
+			}{
+				Mark:     mark,
+				Students: students, Subjects: subjects,
+			})
+		} else {
 
-	renderTemplate("html/create-mark.html", rw, struct {
-		Students []Student
-		Subjects []Subject
-	}{
-		Students: students, Subjects: subjects,
-	})
+			renderTemplate("html/create-mark.html", rw, struct {
+				Students []Student
+				Subjects []Subject
+			}{
+				Students: students, Subjects: subjects,
+			})
+		}
+	}
 }
 
 func renderTemplate(fileName string, rw http.ResponseWriter, data any) {
@@ -149,7 +179,8 @@ func getMarks(rw http.ResponseWriter, r *http.Request) {
 			 INNER JOIN student 
 			 ON mark.student_id = student.id 
 			 INNER JOIN subject 
-			 ON mark.subject_id = subject.id`).Scan(&marks).Error; err != nil {
+			 ON mark.subject_id = subject.id
+			 ORDER BY id ASC`).Scan(&marks).Error; err != nil {
 		http.Error(rw, err.Error(), 400)
 		return
 	}
@@ -181,7 +212,7 @@ func getMarks(rw http.ResponseWriter, r *http.Request) {
 
 func getStudents(rw http.ResponseWriter, r *http.Request) {
 	var students []Student
-	if err := db.Raw("SELECT * FROM student").Scan(&students).Error; err != nil {
+	if err := db.Raw("SELECT * FROM student ORDER BY id ASC").Scan(&students).Error; err != nil {
 		http.Error(rw, err.Error(), 400)
 		return
 	}
@@ -202,7 +233,7 @@ func getStudents(rw http.ResponseWriter, r *http.Request) {
 
 func getSubjects(rw http.ResponseWriter, r *http.Request) {
 	var subjects []Subject
-	if err := db.Raw("SELECT * FROM subject").Scan(&subjects).Error; err != nil {
+	if err := db.Raw("SELECT * FROM subject ORDER BY id").Scan(&subjects).Error; err != nil {
 		http.Error(rw, err.Error(), 400)
 		return
 	}
@@ -227,21 +258,47 @@ func getSubjects(rw http.ResponseWriter, r *http.Request) {
 }
 
 func createStudent(rw http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		name := r.FormValue("name")
+	log.Println("createStudent", r.Method)
+
+	switch r.Method {
+	case http.MethodPost:
 		class := r.FormValue("class")
+		name := r.FormValue("name")
+		id := r.FormValue("id")
 
-		log.Println("new student: name", name, "class", class)
-
-		if err := db.Exec("INSERT INTO student(name, class) VALUES(?, ?)",
-			name, class).Error; err != nil {
-			http.Error(rw, err.Error(), 400)
-			return
+		if id == "" {
+			log.Println("new student: name", name, "class", class)
+			if err := db.Exec("INSERT INTO student (name, class) VALUES (?, ?)", name, class).Error; err != nil {
+				http.Error(rw, err.Error(), 400)
+				return
+			}
+		} else {
+			log.Println("update student: name", name, "class", class, "id", id)
+			if err := db.Exec("UPDATE student SET name = ?, class = ? WHERE id = ?", name, class, id).Error; err != nil {
+				http.Error(rw, err.Error(), 400)
+				return
+			}
 		}
+
 		getStudents(rw, r)
 		return
+
+	case http.MethodGet:
+		if id := r.URL.Query().Get("id"); id != "" {
+			var student Student
+			if err := db.Raw("SELECT * FROM student WHERE id = ?", id).Scan(&student).Error; err != nil {
+				http.Error(rw, err.Error(), 400)
+				return
+			}
+			renderTemplate("html/update-student.html", rw, struct {
+				Student Student
+			}{
+				Student: student,
+			})
+		} else {
+			renderTemplate("html/create-student.html", rw, nil)
+		}
 	}
-	renderTemplate("html/create-student.html", rw, nil)
 }
 
 func createSubject(rw http.ResponseWriter, r *http.Request) {
